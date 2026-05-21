dac25e2706f6e50f2e35baaa94aa5303375ddafa
Cobry696 <colbypynn@gmail.com>
2026-05-21 20:47:17 -0230

    added nix-flatpaks

--- modules/hosts/colby/configuration.nix
+++ modules/hosts/colby/configuration.nix
@@ -1,31 +1,41 @@
-{ self, inputs, ...}: {
+{ self, inputs, ... }:
+{
 
-  flake.nixosModules.colbyConfiguration = { config, pkgs, inputs, ... }: {
-    # Edit this configuration file to define what should be installed on
-    # your system.  Help is available in the configuration.nix(5) man page
-    # and in the NixOS manual (accessible by running ‘nixos-help’).
-      imports =
-        [ # Include the results of the hardware scan.
-          self.nixosModules.colbyHardware
-          self.nixosModules.niri
-        ];
+  flake.nixosModules.colbyConfiguration =
+    {
+      config,
+      pkgs,
+      inputs,
+      ...
+    }:
+    {
+      # Edit this configuration file to define what should be installed on
+      # your system.  Help is available in the configuration.nix(5) man page
+      # and in the NixOS manual (accessible by running ‘nixos-help’).
+      imports = [
+        # Include the results of the hardware scan.
+        self.nixosModules.colbyHardware
+        self.nixosModules.niri
+      ];
       # Enable flakes
-      nix.settings.experimental-features = [ "nix-command" "flakes" ];
-   
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
       # Bootloader.
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
-    
+
       # Use latest kernel.
       boot.kernelPackages = pkgs.linuxPackages_latest;
-    
+
       networking.hostName = "nixos"; # Define your hostname.
       # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    
+
       # Configure network proxy if necessary
       # networking.proxy.default = "http://user:password@proxy:port/";
       # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    
 
       # enable bluetooth
       hardware.bluetooth.enable = true;
@@ -36,19 +46,19 @@
 
       # Enable battery monitoring
       services.upower.enable = true;
-    
+
       # Enable automounting
       services.udisks2.enable = true;
 
       # Set your time zone.
       time.timeZone = "America/St_Johns";
-    
+
       # Select internationalisation properties.
       i18n.defaultLocale = "en_CA.UTF-8";
-    
+
       # Enable the X11 windowing system.
       services.xserver.enable = false;
-    
+
       # Enable the GNOME Desktop Environment.
       #services.displayManager.gdm = {
       #  enable = true;
@@ -58,14 +68,14 @@
       services.displayManager.sddm = {
         enable = true;
         wayland.enable = true;
-	settings.Wayland.CompositorCommand = "${pkgs.kdePackages.kwin}/bin/kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
+        settings.Wayland.CompositorCommand = "${pkgs.kdePackages.kwin}/bin/kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
         theme = "sddm-astronaut-theme";
-      	extraPackages = [ pkgs.sddm-astronaut ];
+        extraPackages = [ pkgs.sddm-astronaut ];
       };
       programs.niri.enable = true;
 
       #services.xserver.desktopManager.gnome.enable = true;
-    
+
       # Configure keymap in X11
       services.xserver.xkb = {
         layout = "us";
@@ -75,10 +85,10 @@
       services.hardware.openrgb = {
         enable = true;
       };
-    
+
       # Enable CUPS to print documents.
       services.printing.enable = true;
-    
+
       # Enable sound with pipewire.
       services.pulseaudio.enable = false;
       security.rtkit.enable = true;
@@ -87,27 +97,30 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-    
+
         # use the example session manager (no others are packaged yet so this is enabled by default,
         # no need to redefine it in your config for now)
         #media-session.enable = true;
       };
-    
+
       # Enable touchpad support (enabled default in most desktopManager).
       # services.xserver.libinput.enable = true;
-    
+
       # Define a user account. Don't forget to set a password with ‘passwd’.
       users.users.colby = {
         isNormalUser = true;
         description = "Colby Pynn";
-        extraGroups = [ "networkmanager" "wheel" ];
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+        ];
         packages = with pkgs; [
-        #  thunderbird
+          #  thunderbird
         ];
       };
 
       services.tailscale.enable = true;
-       
+
       # Install firefox.
       programs.firefox.enable = true;
 
@@ -125,51 +138,51 @@
       };
 
       programs.git = {
-	      enable = true;
+        enable = true;
       };
 
       programs.nix-ld.enable = true;
 
       environment.etc."gitconfig".text = ''
-        [user]
-	        name = Cobry696
-	        email = colbypynn@gmail.com
+                [user]
+        	        name = Cobry696
+        	        email = colbypynn@gmail.com
       '';
-    
+
       # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
 
       # flatpak theming? According to nixos manual
       xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
       xdg.portal.config.common.default = "gtk";
-    
+
       # List packages installed in system profile. To search, run:
       # $ nix search wget
       environment.systemPackages = with pkgs; [
-         #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-         wget
-         ghostty   
-         sddm-astronaut
-         kdePackages.dolphin
-	       wdisplays
-	       usbutils
-	       python3
-         cargo
-         rustc
-         gcc
-         fastfetch
-         p7zip
-         wineWowPackages.base
-         curl
-         unzip
-         btop
-         uv
+        #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        wget
+        ghostty
+        sddm-astronaut
+        kdePackages.dolphin
+        wdisplays
+        usbutils
+        python3
+        cargo
+        rustc
+        gcc
+        fastfetch
+        p7zip
+        wineWowPackages.base
+        curl
+        unzip
+        btop
+        uv
+        ffmpeg-full
 
-         
       ];
 
       home-manager.users.colby = self.homeModules.colbyModule;
-    
+
       # Some programs need SUID wrappers, can be configured further or are
       # started in user sessions.
       # programs.mtr.enable = true;
@@ -177,18 +190,18 @@
       #   enable = true;
       #   enableSSHSupport = true;
       # };
-    
+
       # List services that you want to enable:
-    
+
       # Enable the OpenSSH daemon.
       # services.openssh.enable = true;
-    
+
       # Open ports in the firewall.
       # networking.firewall.allowedTCPPorts = [ ... ];
       # networking.firewall.allowedUDPPorts = [ ... ];
       # Or disable the firewall altogether.
       # networking.firewall.enable = false;
-    
+
       # This value determines the NixOS release from which the default
       # settings for stateful data, like file locations and database versions
       # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -196,7 +209,7 @@
       # Before changing this value read the documentation for this option
       # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
       system.stateVersion = "25.11"; # Did you read the comment?
- 
-  };
+
+    };
 
 }
--- modules/hosts/colby/home.nix
+++ modules/hosts/colby/home.nix
@@ -1,4 +1,5 @@
-{self, inputs, ... }: {
+{ self, inputs, ... }:
+{
 
   flake.homeConfigurations.colby = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
@@ -6,105 +7,113 @@
       self.homeModules.colbyModule
       {
         home.username = "colby";
-	      home.homeDirectory = "/home/colby";
+        home.homeDirectory = "/home/colby";
       }
     ];
   };
 
-  flake.homeModules.colbyModule = { pkgs, lib, ... }: {
-    programs.bash = {
-      enable = true;
-      shellAliases.ls = "ls -la";
-    };
+  flake.homeModules.colbyModule =
+    { pkgs, lib, ... }:
+    {
+      programs.bash = {
+        enable = true;
+        shellAliases.ls = "ls -la";
+      };
 
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        theme = "autumn_night_transparent";
-        editor.cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "underline";
+      programs.helix = {
+        enable = true;
+        defaultEditor = true;
+        settings = {
+          theme = "autumn_night_transparent";
+          editor.cursor-shape = {
+            normal = "block";
+            insert = "bar";
+            select = "underline";
+          };
+          keys = {
+            normal = {
+              Z.Z = ":wq";
+              Z.Q = ":q!";
+              C-s = ":w";
+            };
+          };
         };
-        keys = {
-          normal = {
-            Z.Z = ":wq";
-            Z.Q = ":q!";
-            C-s = ":w";
+
+        extraPackages = with pkgs; [
+          (python3.withPackages (ps: with ps; [ python-lsp-server ]))
+        ];
+
+        languages = with pkgs; {
+          language-server = {
+            pylsp = {
+              command = "pylsp";
+            };
+          };
+
+          language = [
+            {
+              name = "nix";
+              auto-format = true;
+              formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+            }
+            {
+              name = "python";
+              auto-format = true;
+              language-servers = [
+                "ruff"
+                "pylsp"
+              ]; # or "pyright"
+
+              # Optional: Configure Ruff formatter
+              formatter.command = "${pkgs.ruff}/bin/ruff";
+              formatter.args = [
+                "format"
+                "-"
+              ];
+
+              # Optional: Configure pylsp settings
+              # Note: pylsp configuration is often handled via pylsp's own config files
+              # or by passing arguments if the LSP supports it.
+            }
+          ];
+        };
+
+        #"language-server.pylsp".command = "${pkgs.python3Packages.python-lsp-server}/bin/pylsp";
+
+        themes = {
+          autumn_night_transparent = {
+            "inherits" = "autumn_night";
+            "ui.background" = { };
           };
         };
       };
 
-      extraPackages = with pkgs; [
-        (python3.withPackages (ps: with ps; [ python-lsp-server ]))
+      programs.zellij.enable = true;
+
+      programs.yazi.enable = true;
+
+      home.packages = [
+        pkgs.blender
+        pkgs.brave
+        pkgs.discord
+        pkgs.spotify
+        pkgs.heroic
+        pkgs.vscode
+        pkgs.gamescope
+        pkgs.grsync
+        pkgs.openrgb
+        pkgs.prismlauncher
       ];
 
-      languages = with pkgs; {
-        language-server = {
-          pylsp = {
-            command = "pylsp";
-          };
-        };
-
-        language = [
-          {
-            name = "nix";
-            auto-format = true;
-            formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-          }
-          {
-            name = "python";
-            auto-format = true;
-            language-servers = [ "ruff" "pylsp" ]; # or "pyright"
-            
-            # Optional: Configure Ruff formatter
-            formatter.command = "${pkgs.ruff}/bin/ruff";
-            formatter.args = [ "format" "-" ];
-            
-            # Optional: Configure pylsp settings
-            # Note: pylsp configuration is often handled via pylsp's own config files 
-            # or by passing arguments if the LSP supports it.
-          }
-        ];
+      home.pointerCursor = {
+        gtk.enable = true;
+        x11.enable = true;
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 16;
       };
 
-      #"language-server.pylsp".command = "${pkgs.python3Packages.python-lsp-server}/bin/pylsp";
-
-      themes = {
-        autumn_night_transparent = {
-          "inherits" = "autumn_night";
-          "ui.background" = { };
-        };
-      };
+      home.stateVersion = "25.11";
     };
-
-    programs.zellij.enable = true;
-
-    programs.yazi.enable = true;
-
-    home.packages = [
-      pkgs.blender
-      pkgs.brave
-      pkgs.discord
-      pkgs.spotify
-      pkgs.heroic
-      pkgs.vscode
-      pkgs.gamescope
-      pkgs.grsync
-      pkgs.openrgb
-	    pkgs.prismlauncher
-    ];
-
-    home.pointerCursor = {
-      gtk.enable = true;
-      x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 16;
-    };
-
-    home.stateVersion = "25.11";
-  };
 
 }
